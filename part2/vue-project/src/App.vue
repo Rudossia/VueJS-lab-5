bb0c9b24c383aca57f585fbce35f7e5b55c9dce9
Bessonov Rudolf <rebunok@gmail.com>
2026-04-24 20:21:04 +0300

    vue js

---
+++ part2/vue-project/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Clicker from './components/Clicker.vue'
+
+function handleButtonClicked(n) {
+  alert(`Clicker ${n} clicked!`)
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>100 Clickers</h1>
+
+    <div class="grid">
+      <Clicker
+        v-for="n in 100"
+        :key="n"
+        :amount="1"
+        @button-clicked="handleButtonClicked(n)"
+      >
+        <button>Нажми</button>
+      </Clicker>
+    </div>
+  </div>
+</template>
---
+++ part2/vue-project/src/components/Clicker.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  amount: {
+    type: Number,
+    default: 1
+  }
+})
+
+const emit = defineEmits(['button-clicked'])
+
+const count = ref(0)
+
+function increaseCount() {
+  count.value += props.amount
+  emit('button-clicked')
+}
+</script>
+
+<template>
+  <div class="clicker">
+    <p>Очки: {{ count }}</p>
+
+    <div class="click-area" @click="increaseCount">
+      <slot>
+        <button>Нажми</button>
+      </slot>
+    </div>
+  </div>
+</template>
